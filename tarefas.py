d4e3a6fb231f559a23982d22c9a39e95c7005c0f
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-03-28 21:34:32 -0300

    Cria arq tarefas.py

---
+++ tarefas.py
@@ -0,0 +1,37 @@
+# Tarefa 1:
+ 
+# Crie uma função que recebe dois números e retorne a soma deles.
+ 
+# Execute a função passando dois números e guarde o resultado dela em uma variável.
+ 
+# Em seguida, crie outra função para imprimir o valor dessa variável no terminal.  
+ 
+# Após concluir, faça um commit deste código no seu repositório remoto.
+
+# def somar_numeros(numA, numB):
+#     soma = numA + numB * 2
+#     return soma
+
+# resultado_da_soma = somar_numeros(5, 10)
+
+# def exibir_soma_resultados(resultado):
+#     print(resultado)
+
+# exibir_soma_resultados
+
+# Tarefa 2*:
+ 
+# Implemente uma função que receba dois números e 
+# retorne o produto(*) deles. 
+# Execute a função com dois números de sua escolha e 
+# armazene o resultado em uma variável. 
+# Reutilize a função de imprimir para mostrar o valor dessa variável no terminal.
+ 
+# Após concluir, faça um segundo commit deste código no seu repositório remoto.
+
+def multiplica_numeros(num1, num2):
+    multiplica = num1 * num2
+    return multiplica
+
+resultado_do_produto = multiplica_numeros (5, 10)
+print(resultado_do_produto)
